caba1c25ea1d7462556902384924a8954ead0b4d
voicekapp1-ux <voicekapp1@gmail.com>
2026-03-05 02:10:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/omar-ewies-portfolio/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        base: '/moe-portfolio/',
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
